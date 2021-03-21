0a6debd5741b0dd7e0a71a0cef0c296cde44b2d6
NoOourAn <nouranaborwash@gmail.com>
2021-03-21 12:04:32 +0200

    db connection async await to avoid buffering issues

--- db_connection/mongodb.js
+++ db_connection/mongodb.js
@@ -2,20 +2,37 @@ const mongoose = require('mongoose');
 mongoose.set('bufferCommands', false);
 mongoose.set('autoCreate', false);
 
-mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:/e-commerce-DB',
-                {
-                    useNewUrlParser: true, 
-                    useUnifiedTopology: true , 
-                    useFindAndModify:false,
-                    useCreateIndex: true
-                });
+//DB Connection
+getConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:/e-commerce-DB',
+        {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true , 
+            useFindAndModify:false,
+            useCreateIndex: true
+        });
+        console.log('Database connected Successfully!');
+    } catch (err) {
+      console.log('Database connection Failed!');
+    }
+  };
+  
+getConnection();
 
 
-const db = mongoose.connection;
+///// XXXXX this way of connection cause buffering errors 
+///// XXXXX the models used before the connection established !!
+// mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:/e-commerce-DB',
+//                 {
+//                     useNewUrlParser: true, 
+//                     useUnifiedTopology: true , 
+//                     useFindAndModify:false,
+//                     useCreateIndex: true
+//                 });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'Database connection error:'));
+// db.once('open', ()=> {
+// console.log("Database connected!")
 
-
-db.on('error', console.error.bind(console, 'Database connection error:'));
-db.once('open', ()=> {
-console.log("Database connected!")
-
-});
+// });
