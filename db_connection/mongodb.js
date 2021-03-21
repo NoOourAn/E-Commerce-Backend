1881c9249ceb2809dde9690b40b0b5fd1927b709
NoOourAn <nouranaborwash@gmail.com>
2021-03-21 11:27:20 +0200

    db connection async await

--- db_connection/mongodb.js
+++ db_connection/mongodb.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:/e-commerce-DB',
+                {
+                    useNewUrlParser: true, 
+                    useUnifiedTopology: true , 
+                    useFindAndModify:false,
+                    useCreateIndex: true
+                });
 
-mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:27017/ecom-test', { useNewUrlParser: true, useUnifiedTopology: true },
-// 'mongodb+srv://ahmed:791995@e-commerce.uldwf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    (err) => {
-        if (err) {
-            console.warn("failed to connect to MongoDB")
-            console.error(err);
-            process.exit(1);
-        }
-        console.info(`connected to DB successfully`);
-    });
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Database connection error:'));
+db.once('open', ()=> {
+console.log("Database connected!")
+
+});
