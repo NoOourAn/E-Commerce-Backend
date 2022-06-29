bf601730f20d27115f15fe894415a8e1a7cfd371
NoOourAn <nouranaborwash@gmail.com>
2022-06-29 11:42:47 +0200

    edit env 2

--- routers/user.js
+++ routers/user.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user')
 const authenticationMiddleware = require('../middlewares/authentication');
 const userRouter = new express.Router();
-var multer  = require('multer');
+var multer = require('multer');
 const multerS3 = require('multer-s3');
 const fs = require('fs');
 const AWS = require('aws-sdk');
@@ -16,7 +16,7 @@ const ID = process.env.AWS_accessKeyId;
 const SECRET = process.env.AWS_secretAccessKey;
 
 // The name of the bucket that you have created
-const BUCKET_NAME = 'marketo-e-commerce';
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
 // initialize the S3 interface
 const s3 = new AWS.S3({
@@ -27,36 +27,36 @@ const s3 = new AWS.S3({
 ///// to upload to AWS S3 
 const upload = multer({
     storage: multerS3({
-      s3: s3,
-      bucket: BUCKET_NAME,
-      acl: 'public-read',
-      cacheControl: 'max-age=31536000',
-      contentType: multerS3.AUTO_CONTENT_TYPE,
-      metadata: function (req, file, cb) {
-        cb(null, {fieldName: file.fieldname});
-      },
-      key: function (req, file, cb) {
-        //const key = `user-profile-images/${process.env.NODE_ENV}_${Date.now().toString()}${path.extname(file.originalname)}`
-        var filetype = '';
-        if(file.mimetype === 'image/gif') {
-            filetype = 'gif';
+        s3: s3,
+        bucket: BUCKET_NAME,
+        acl: 'public-read',
+        cacheControl: 'max-age=31536000',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            //const key = `user-profile-images/${process.env.NODE_ENV}_${Date.now().toString()}${path.extname(file.originalname)}`
+            var filetype = '';
+            if (file.mimetype === 'image/gif') {
+                filetype = 'gif';
+            }
+            if (file.mimetype === 'image/png') {
+                filetype = 'png';
+            }
+            if (file.mimetype === 'image/jpeg') {
+                filetype = 'jpg';
+            }
+            const key = 'image-' + Date.now() + '.' + filetype;
+            cb(null, key);
         }
-        if(file.mimetype === 'image/png') {
-            filetype = 'png';
-        }
-        if(file.mimetype === 'image/jpeg') {
-            filetype = 'jpg';
-        }
-        const key = 'image-' + Date.now() + '.' + filetype;
-        cb(null, key);
-      }
     }),
-  });
+});
 
 
 
 ///admin can get all users
-userRouter.get('/getUsers', async(req, res) => {
+userRouter.get('/getUsers', async (req, res) => {
     try {
         const users = await User.find({});
         const obj = {
@@ -65,16 +65,16 @@ userRouter.get('/getUsers', async(req, res) => {
         }
         res.send(obj);
     } catch (err) {
-        res.json({ statusCode: 422 ,success: false, message: err.message });
+        res.json({ statusCode: 422, success: false, message: err.message });
 
     }
 })
 
-userRouter.post('/reg', upload.single('file') ,async(req, res) => { // the registration router
+userRouter.post('/reg', upload.single('file'), async (req, res) => { // the registration router
     try {
         const { username, email, password } = req.body;
         if (username && email && password) {
-            if(req.file){
+            if (req.file) {
                 //for mongo database      
                 req.body.imgUrl = req.file.location;
                 req.body.imgName = req.file.key;
@@ -89,13 +89,13 @@ userRouter.post('/reg', upload.single('file') ,async(req, res) => { // the regis
                 user: user
             }
             res.send(obj)
-        } else throw new Error("username , email , password are required")   
+        } else throw new Error("username , email , password are required")
     } catch (err) {
-        res.json({ statusCode: 422 ,success: false, message: "Duplicated User" });
+        res.json({ statusCode: 422, success: false, message: "Duplicated User" });
     }
 })
 
-userRouter.post('/login', async(req, res) => { // the login router
+userRouter.post('/login', async (req, res) => { // the login router
     try {
         const { username, password } = req.body;
         const user = await User.findOne({ username }).exec();
@@ -103,9 +103,9 @@ userRouter.post('/login', async(req, res) => { // the login router
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) throw new Error("wrong username or password");
         const token = jwt.sign({ id: user.id }, process.env.SecretKey);
-        res.json({ success: true, token , user });
+        res.json({ success: true, token, user });
     } catch (err) {
-        res.json({ statusCode: 422 ,success: false, message: "username or password is invalid" });
+        res.json({ statusCode: 422, success: false, message: "username or password is invalid" });
 
     }
 });
@@ -113,7 +113,7 @@ userRouter.post('/login', async(req, res) => { // the login router
 
 userRouter.use(authenticationMiddleware) // this will make only the logged user to use the below functions
 
-userRouter.get('/myProfile', async(req, res) => { // will show the info of my profile
+userRouter.get('/myProfile', async (req, res) => { // will show the info of my profile
     try {
         const user = await User.findOne({ _id: req.signedData.id }, { password: 0 }); // password=0 means dont show it to me
         const obj = {
@@ -122,17 +122,17 @@ userRouter.get('/myProfile', async(req, res) => { // will show the info of my pr
         }
         res.send(obj);
     } catch (err) {
-        res.json({ statusCode: 404 ,success: false, message: err.message });
+        res.json({ statusCode: 404, success: false, message: err.message });
 
     }
 })
 
 /////update profile data
-userRouter.patch('/profileUpdate',async(req, res) => { // update router for user
+userRouter.patch('/profileUpdate', async (req, res) => { // update router for user
     try {
         const { username, email } = req.body;
         const user = await User.updateOne({ _id: req.signedData.id }, { $set: { username: username, email: email } });
-        
+
         const obj = {
             success: true,
             message: "profile was edited succesfully",
@@ -145,14 +145,14 @@ userRouter.patch('/profileUpdate',async(req, res) => { // update router for user
     }
 })
 /////////update profile img
-userRouter.patch('/imgUpdate', upload.single('file') ,async(req, res) => { // update image router for user
+userRouter.patch('/imgUpdate', upload.single('file'), async (req, res) => { // update image router for user
     try {
-        if(req.file){
+        if (req.file) {
             //for mongo database      
             req.body.imgUrl = req.file.location;
             req.body.imgName = req.file.key;
             // const user = await User.findByIdAndUpdate(req.signedData.id , { imgUrl:req.body.imgUrl,imgName:req.body.imgName  }, { returnOriginal: true })
-            const user = await User.updateOne({ _id: req.signedData.id }, { $set: { imgUrl:req.body.imgUrl,imgName:req.body.imgName } });
+            const user = await User.updateOne({ _id: req.signedData.id }, { $set: { imgUrl: req.body.imgUrl, imgName: req.body.imgName } });
         }
         const obj = {
             success: true,
@@ -166,7 +166,7 @@ userRouter.patch('/imgUpdate', upload.single('file') ,async(req, res) => { // up
     }
 })
 
-userRouter.delete('/profileDelete', async(req, res) => {
+userRouter.delete('/profileDelete', async (req, res) => {
     try { // this router will delete the user profile and its data
         const user = await User.deleteOne({ _id: req.signedData.id }); //delete the user it self
         const obj = {
